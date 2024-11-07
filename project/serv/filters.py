0461759cb0beaac8650d0032189b04e870b53cfb
Кирилл Воскресенов <132572547+kirillVoskresenov@users.noreply.github.com>
2024-11-07 23:28:13 +0300

    Add files via upload

---
+++ project/serv/filters.py
@@ -0,0 +1,30 @@
+import django_filters
+from django import forms
+from django_filters import FilterSet
+from .models import *
+
+class PostFilter(FilterSet):
+
+    date = django_filters.DateFilter(
+        field_name='article_date',
+        widget=forms.DateInput(attrs={'type': 'date'}),
+        lookup_expr='gte',
+        label='Date (beginning with)'
+    )
+
+
+    class Meta:
+        model = Post
+        fields = {
+           'author': ['exact'],
+           'title': ['icontains']
+       }
+
+class CommFilter(FilterSet):
+
+    class Meta:
+        model = Comment
+        fields = {
+           'user': ['exact'],
+           'text': ['icontains']
+       }
---
+++ project/serv/urls.py
@@ -0,0 +1,28 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from .views import AuthView, PostsList, PostDetail, PostCreate, PostUpdate, PostDelete, \
+   PostSearch, CommentSearch, CommentList, CommentDetail, CommentDelete, CommentUpdate, \
+   CommentCreate, upload_media, accept, reject
+
+urlpatterns = [
+   path('accounts/profile/', AuthView.as_view()),
+   path('', PostsList.as_view(), name='startpage'),
+   path('<int:pk>', PostDetail.as_view(), name='detail'),
+   path('create/', PostCreate.as_view(), name='create'),
+   path('<int:pk>/update/', PostUpdate.as_view(), name='edit'),
+   path('<int:pk>/delete/', PostDelete.as_view(), name='delete'),
+   path('search/', PostSearch.as_view),
+   path('comment_search/', CommentSearch.as_view),
+   path('comments/', CommentList.as_view(), name="comment_list"),
+   path('comments/create/<int:pk>/', CommentCreate.as_view(), name="comment_create"),
+   path('comments/<int:pk>/', CommentDetail.as_view(), name="comment_detail"),
+   path('<int:pk>/update/', CommentUpdate.as_view(), name='comment_update'),
+   path('<int:pk>/delete/', CommentDelete.as_view(), name='comment_delete'),
+   path('upload/', upload_media, name='upload'),
+   path('accept/<int:pk>', accept, name='accept'),
+   path('reject/<int:pk>', accept, name='reject')
+]
+
+if settings.DEBUG:
+   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
